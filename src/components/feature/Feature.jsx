581099357c666432ed11387347930244effb3b22
George Irungu <ngothogeorgedesign@gmail.com>
2023-07-26 09:55:31 +0300

    create feature section

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./feature.css";
 
-const Feature = () => {
-  return <div>Feature</div>;
-};
+const Feature = ({ title, text }) => (
+  <div id='feature'>
+    <div className='feature-title'>
+      <div className='color' />
+      <h1>{title}</h1>
+    </div>
+    <div></div>
+    <div className='feature-text'>
+      <h1>{text}</h1>
+    </div>
+  </div>
+);
 
 export default Feature;
