3ac1950c3ccdeff12642019f195c008e03431905
George Irungu <ngothogeorgedesign@gmail.com>
2023-07-26 13:03:14 +0300

    create possibility section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./possibility.css";
+import possibilityImage from "../../images/possibility.png";
 
 const Possibility = () => {
-  return <div>Possibility</div>;
+  return (
+    <div id='possibility'>
+      <div className='possibility-image'>
+        <img src={possibilityImage} />
+      </div>
+      <div className='possibility-content'>
+        <h4>Request Early Access to Get Started</h4>
+        <h1 className='gradient__text'>
+          The possibilities are <br /> beyond your imagination
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <h4 className='request'>Request Early Access to Get Started</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Possibility;
