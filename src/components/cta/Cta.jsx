30256a55d5123b6d819d816336ce9465eb454260
George Irungu <ngothogeorgedesign@gmail.com>
2023-07-26 13:27:20 +0300

    create cta section

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./cta.css";
 
 const Cta = () => {
-  return <div>Cta</div>;
+  return (
+    <div id='cta'>
+      <div className='cta-left'>
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className='cta-right'>
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cta;
